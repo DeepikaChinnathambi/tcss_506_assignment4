43f8fbde45b5b0c1b2213ac28d43d93bd20b483c
Deepika Chinnathambi <deepika14.cvl@gmail.com>
2025-05-11 20:12:57 -0700

    Assignment4 completed code

--- default/app.py
+++ default/app.py
@@ -8,11 +8,6 @@ app = Flask(__name__)
 app.secret_key = 'your_secret_key_here'
 @app.route('/')
 def index():
-    # Set default session values
-    session.setdefault('city', 'Tacoma')
-    session.setdefault('state', 'WA')
-    session.setdefault('category', 'restaurants')
-    # Get query parameters from the URL
     session['city'] = 'Tacoma'
     session['state'] = 'WA'
     session['category'] = 'restaurants'
@@ -23,20 +18,25 @@ def index():
             session['state'] = request.args.get('state')
         if request.args.get('category'):
             session['category'] = request.args.get('category')
-        
-    # Create a data object with business information
-    # Fetch location data from the API (cache-only for now)
+    
+    hotels = fetch_tripadvisor_data(session['city'], session['state'], session['category'], use_cache_only=True)
 
+    # # Filter out bad entries
+    # cleaned_restaurants = [r for r in restaurants if isinstance(r, dict)]
+    print("Category = ", session['category'])
+
+    print("**********Hotels ***** = ", hotels)
+    # Create a data object with business information
     location_data = {
-        'restaurants': fetch_tripadvisor_data(session['city'], session['state'], session['category'],use_cache_only=True),
-        'hotels': fetch_tripadvisor_data(session['city'], session['state'], session['category'],use_cache_only=True),
-        'attractions':fetch_tripadvisor_data(session['city'], session['state'], session['category'],use_cache_only=True),
+        'restaurants': fetch_tripadvisor_data(session['city'], session['state'], session['category'], use_cache_only=True),
+        'hotels': fetch_tripadvisor_data(session['city'], session['state'], session['category'], use_cache_only=True),
+        'attractions': fetch_tripadvisor_data(session['city'], session['state'], session['category'], use_cache_only=True),
     }
     return render_template('index.html', city=session['city'], state=session['state'], category=session['category'], location_data=location_data)
 
 @app.route("/about")
 def about():
     return render_template("about.html")
-
+    
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
